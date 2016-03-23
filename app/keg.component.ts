105178f5bf9a4e374725cd07bf260bdb05d59f8e
Hillary Hintzen and Kyle Derrick <kderrick1988@gmail.com>
2016-03-23 16:16:22 -0700

    add sell pint functionality

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -5,10 +5,24 @@ import { Keg } from './keg.model';
   selector: 'keg-display',
   inputs: ['keg'],
   template: `
-  <h3>{{keg.name}} {{keg.brand}} {{"$" + keg.price}} {{keg.abv + "%"}}</h3>
+  <h3>{{keg.name}} {{keg.brand}} {{"$" + keg.price + ".00"}} {{keg.abv + "%ABV"}} {{"Pints left: " + keg.pintsLeft}}</h3>
+  <button (click)="soldPint(keg)"> Sell Pint </button>
   `
 })
 
 export class KegComponent {
   public keg: Keg;
+  soldPint(selectedKeg: Keg) {
+    if((this.keg.pintsLeft - 1) <= 10 ) {
+      alert("Keg is getting low");
+      this.keg.pintsLeft -= 1;
+      console.log("Keg is getting low");
+    } else if (this.keg.pintsLeft === 0) {
+      alert("KEG IS EMPTY!!!");
+      console.log("KEG IS EMPTY!!!")
+    } else {
+      this.keg.pintsLeft -= 1;
+      console.log("Filling a pint");
+    }
+  }
 }
--- app/keg.model.ts
+++ app/keg.model.ts
@@ -1,7 +1,7 @@
 export class Keg {
   public pintsLeft: number = 124;
   constructor(public name: string, public brand: string, public price: number, public abv: number, public id: number) {}
-  soldPint() {
-    this.pintsLeft -= 1;
-  }
+  // soldPint(selectedKeg: Keg) {
+  //   this.pintsLeft -= 1;
+  // }
 }
